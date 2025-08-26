3870729e8654e01c62442da7cb5d5aa17818cc28
Princiya Marina Sequeira <princiya.marina@gmail.com>
2025-08-26 12:41:05 +0200

    Update SITEURL to GitHub Pages URL

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://princiya.com/newblog'
+SITEURL = 'https://princiya.github.io/newblog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
